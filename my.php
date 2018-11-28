bf753c3c130a4c7e7c455ed8ac3a9d42cf715a62
zengxianao <1018975516@qq.com>
2018-11-28 18:28:13 +0800

    my first composer init; add class Eecho/Aicom;

---
+++ my.php
@@ -0,0 +1,7 @@
+<?php
+require_once './vendor/autoload.php';
+
+use Eecho\Aicom;
+
+$a = new Aicom();
+$a->echo_m();
---
+++ src/Aicom.php
@@ -0,0 +1,13 @@
+<?php
+namespace Eecho;
+/**
+* echo 
+*/
+class Aicom
+{
+	
+	public function echo_m()
+	{
+		echo 'this is your first composer pack;';
+	}
+}
